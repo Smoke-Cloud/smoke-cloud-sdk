41ba7cda7d1addcb9b395b8651d42be99e815ec1
Jake O'Shannessy <joshannessy@affinity-fire.com>
2024-01-16 10:37:22 +1100

    Create smoke-cloud-sdk

---
+++ api/api.ts
@@ -0,0 +1,414 @@
+import * as uuid from "uuid";
+import { type CurrentUsage, RunEntry, RunId, coresToInstance, NCores, InstanceType, Phase, ProgressInfo, PublicRunningStatus, User } from "./coreTypes.ts";
+import { UserOrgInfo } from "./credentials.ts";
+import { DataVector, RunData } from "./getS3CSVData.ts";
+import { AuthProvider } from "./authProviders/mod.ts";
+export * from "./credentials.ts";
+export * from "./coreTypes.ts";
+export * from "./authProviders/mod.ts";
+export class ApiError extends Error { }
+
+export interface ScApiErrorResponse {
+  errors: ScApiErrorObject[];
+}
+
+export interface ScApiErrorObject {
+  id?: string;
+  links?: {
+    about?: string;
+    type?: string;
+  };
+  status?: string;
+  code: string;
+  title?: string;
+  detail?: string;
+  source?: {
+    pointer?: string;
+    parameter?: string;
+    header?: string;
+  };
+  meta?: object;
+
+}
+
+export interface RunFilter {
+  /// unix time in milliseconds
+  updatedSince?: number;
+  chid?: string;
+}
+
+export class ApiClient {
+  private stage = "v3";
+  public api_endpoint = new URL("https://api.smokecloud.io");
+  #passwordToken?: string;
+  // TODO: passing in the refreshToken callback is a bit hacky, should try and
+  // remove it.
+  constructor(private authProvider: AuthProvider, public accountId: string, options?: {
+    stage?: string,
+    api_endpoint?: string
+  }) {
+    if (options?.stage) {
+      this.stage = options.stage;
+    }
+    if (options?.api_endpoint) {
+      this.api_endpoint = new URL(options.api_endpoint);
+    }
+  }
+
+  private async request(method: string, path: string, useAccountId: boolean, body?: ReadableStream<Uint8Array> | string, opts?: any, queryParams?: Record<string, string>) {
+    if (path.startsWith(`${this.stage}`)) {
+      path.slice(this.stage.length)
+    }
+    const token = await this.authProvider.acquireToken();
+    const p = opts ? opts : {};
+    p.method = method;
+    const headers: Headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Request-Headers", "Location");
+    headers.append(
+      "Authorization",
+      `Bearer ${token}`,
+    );
+    if (p.headers) {
+      if (typeof p.headers.entries === "function") {
+        const iter = p.headers.entries();
+        for (const [k, v] of iter) {
+          if (typeof k === "string" && typeof v === "string") {
+            headers.append(k, v);
+          }
+        }
+      }
+    }
+    p.headers = headers;
+    if (body) {
+      p.body = body;
+    }
+    if (path.length !== 0 && !path.startsWith("/")) {
+      path = `/${path}`;
+    }
+    const url = new URL(`${this.api_endpoint}${path}`);
+    if (useAccountId) {
+      url.pathname = `/${this.stage}/orgs/${this.accountId}${url.pathname}`;
+    } else {
+      url.pathname = `/${this.stage}${url.pathname}`;
+    }
+
+    if (queryParams) {
+      for (const k of Object.keys(queryParams)) {
+        url.searchParams.append(k, queryParams[k])
+      }
+    }
+    const request = new Request(`${url}`, p);
+    try {
+      // TODO: this has issues with redirects as the authentication header is
+      // lost
+      const response = await fetch(request);
+      return response;
+    } catch (e) {
+      console.warn(`Failed: apiRequest[${method}]: ${url}`);
+      throw e;
+    }
+  }
+
+  async apiRequestRoot(method: string, path: string, body?: ReadableStream<Uint8Array> | string, opts?: any, queryParams?: Record<string, string>) {
+    return await this.request(method, path, false, body, opts, queryParams);
+  }
+
+  public runs(filter?: RunFilter & { limit?: number }): RunEntryIter {
+    return new RunEntryIter(this, filter);
+  }
+
+  public async status(): Promise<PublicRunningStatus[]> {
+    const path = `/orgs/${this.accountId}/running_status`;
+    const resp = await this.apiRequestRoot("GET", path);
+    if (resp.ok) {
+      return (await resp.json()).data;
+    } else {
+      throw new Error((await resp.json()).error)
+    }
+  }
+
+  public async run(runId: RunId): Promise<RunEntry> {
+    const path = `/runs/${runId}`;
+    const resp = await this.apiRequestRoot("GET", path);
+    if (resp.ok) {
+      return (await resp.json()).data;
+    } else {
+      throw new Error((await resp.json()).error)
+    }
+  }
+
+  public async progress(runId: RunId): Promise<ProgressInfo> {
+    const path = `/runs/${runId}/progress`;
+    const resp = await this.apiRequestRoot("GET", path);
+    if (resp.ok) {
+      return (await resp.json()).data;
+    } else {
+      throw new Error((await resp.json()).error)
+    }
+  }
+
+  public async confirmClosed(runId: RunId): Promise<boolean> {
+    let run = await this.run(runId);
+    while (run.open) {
+      run = await this.run(runId);
+      await (new Promise((resolve) => setTimeout(resolve, 2 * 1000)));
+    }
+    return !run.open;
+  }
+
+  public async err(runId: RunId, location: Phase.Storage | Phase.Running, opts?: { range?: string }): Promise<string> {
+    const path = `/runs/${runId}/err?phase=${location}`;
+    const headers = new Headers();
+    if (opts?.range) {
+      headers.set("Range", opts?.range)
+    }
+    const resp = await this.apiRequestRoot("GET", path, undefined, {
+      headers
+    });
+    return resp.text();
+  }
+
+  public async errBytes(runId: RunId, location: Phase.Storage | Phase.Running, opts?: { range?: string }): Promise<Blob> {
+    const path = `/runs/${runId}/err?phase=${location}`;
+    const headers = new Headers();
+    if (opts?.range) {
+      headers.set("Range", opts?.range)
+    }
+    const resp = await this.apiRequestRoot("GET", path, undefined, {
+      headers
+    });
+    return resp.blob();
+  }
+
+  public async input(runId: RunId, location: Phase.Storage | Phase.Running, opts?: { range?: string }): Promise<string> {
+    const path = `/runs/${runId}/input?phase=${location}`;
+    const headers = new Headers();
+    if (opts?.range) {
+      headers.set("Range", opts?.range)
+    }
+    const resp = await this.apiRequestRoot("GET", path, undefined, {
+      headers
+    });
+    return resp.text();
+  }
+
+  public async zip(runId: RunId): Promise<ReadableStream<Uint8Array> | null> {
+    const path = `/runs/${runId}/zip`;
+    const resp = await this.apiRequestRoot("GET", path);
+    return resp.body;
+  }
+
+
+  public async data(runId: string, location: Phase, csvtype: string, value: string): Promise<DataVector<number, number>> {
+    const path = `/runs/${runId}/data`;
+    const resp = await this.apiRequestRoot("GET", path, undefined, undefined, {
+      phase: location,
+      csvtype,
+      value
+    });
+    const r = await resp.json();
+    return r.data;
+  }
+
+
+  public async runData(runId: string): Promise<RunData> {
+    const path = `/runs/${runId}/data/run`;
+    const resp = await this.apiRequestRoot("GET", path);
+    const r = await resp.json();
+    return r.data;
+  }
+
+  public async newRun(startParams: SubmitStartParams, input: ReadableStream<Uint8Array> | string): Promise<RunEntry> {
+    if (!startParams.chid || startParams.chid.length === 0) {
+      throw new Error("no CHID provided");
+    }
+    const path = `/orgs/${this.accountId}/runs`;
+    const instanceType = typeof startParams.instance_type === "number" ? coresToInstance(startParams.instance_type) : startParams.instance_type;
+    const params: Record<string, string> = {
+      chid: startParams.chid,
+      fds_version: startParams.fds_version,
+      instance_type: instanceType
+    }
+    if (startParams.project) {
+      params.project = startParams.project;
+    }
+    try {
+      // Post the submission info.
+      const resp = await this.apiRequestRoot("POST", path, input, {
+        headers: new Headers({
+          // TODO: this is not actually used currently
+          "Idempotency-Key": uuid.v4(),
+          "Content-Type": "application/octet-stream"
+        })
+      }, params);
+      if (!resp.ok) {
+        if (resp.status === 409) {
+          // There was a conflict, this means either it failed due to the
+          // idempotency key or there is already an open model.
+          // const result = { run_id: await resp.text(), existing: true };
+          // return result;
+          throw new Error(await resp.text())
+        } else {
+          throw new Error(await resp.text())
+        }
+      }
+      return (await resp.json()).data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async load(): Promise<CurrentUsage> {
+    const path = `/orgs/${this.accountId}/load`;
+    const resp = await this.apiRequestRoot("GET", path);
+    return (await resp.json()).data;
+  }
+
+  public async me(): Promise<User> {
+    const path = "/me";
+    const resp = await this.apiRequestRoot("GET", path);
+    return (await resp.json()).data;
+  }
+
+  public org(): Promise<UserOrgInfo | undefined> {
+    return this.authProvider.org();
+  }
+  follow(runId: string): ReadableStream<Uint8Array> {
+    const follower = new Follower(this, runId);
+    // TODO: ReadableStream.from is not widely available enough yet
+    // return ReadableStream.from(follower);
+    return readableStreamFromAsyncIterator(follower[Symbol.asyncIterator]());
+  }
+  async stop(runId: string) {
+    const result = await this.apiRequestRoot("PUT", `/runs/${runId}/stop`);
+    return result.text();
+  }
+  async kill(runId: string) {
+    const result = await this.apiRequestRoot("PUT", `/runs/${runId}/kill`);
+    return result.text();
+  }
+}
+
+class Follower implements AsyncIterable<Uint8Array> {
+  private closed = false;
+  private nRead = 0;
+  constructor(private client: ApiClient, public runId: RunId) {
+  }
+  async *[Symbol.asyncIterator](): AsyncIterableIterator<Uint8Array> {
+    while (!this.closed) {
+      // TODO: would be better to get the --follow option working on the server
+      await (new Promise((resolve) => setTimeout(resolve, 2 * 1000)));
+      const run = await this.client.run(this.runId);
+      this.closed = !run.open;
+      // TODO: the server should be able to handle this element of phase
+      const phase = closed ? Phase.Storage : Phase.Running;
+      const s = await this.client.errBytes(this.runId, phase, { range: `bytes=${this.nRead}-` });
+      if (!s) break;
+      if (s.slice(this.nRead).size) {
+        yield new Uint8Array(await s.slice(this.nRead).arrayBuffer());
+        this.nRead += s.slice(this.nRead).size;
+      }
+    }
+  }
+}
+
+
+
+export interface PagedResponse<T> {
+  data: T[],
+  links?: {
+    next?: string
+  }
+}
+
+// TODO: add support for more filter options
+export class RunEntryIter implements AsyncIterable<RunEntry> {
+  private nextUrl?: string;
+  constructor(private client: ApiClient, filter?: RunFilter & { limit?: number }) {
+    const params = new URLSearchParams();
+    if (filter?.updatedSince) {
+      params.set("from_time", filter?.updatedSince.toString());
+    }
+    if (filter?.chid) {
+      params.set("chid", filter?.chid);
+    }
+    if (filter?.limit) {
+      params.set("limit", filter?.limit.toString());
+    }
+    if (params.size > 0) {
+      this.nextUrl = `/orgs/${this.client.accountId}/runs?${params.toString()}`;
+    } else {
+      this.nextUrl = `/orgs/${this.client.accountId}/runs`;
+    }
+
+  }
+  async *[Symbol.asyncIterator](): AsyncIterableIterator<RunEntry> {
+    while (1) {
+      if (this.nextUrl) {
+        const qs: Record<string, string> = {};
+        const resp = await this.client.apiRequestRoot("GET", this.nextUrl, undefined, undefined, qs);
+        if (resp.ok) {
+          const result: PagedResponse<RunEntry> = await resp.json();
+          for (const r of result.data) {
+            yield r;
+          }
+          if (result.links?.next) {
+            this.nextUrl = result.links.next;
+          } else {
+            break;
+          }
+        } else {
+          throw new Error(await resp.json());
+
+        }
+      }
+    }
+  }
+}
+
+export interface SubmitStartParams {
+  project?: string,
+  chid: string,
+  fds_version: string,
+  instance_type: NCores | InstanceType,
+}
+
+export interface UploadProgressResult {
+  id: string,
+  status: UploadStatus,
+}
+
+export enum UploadStatus {
+  Running = "running",
+  Completed = "completed",
+  Failed = "failed",
+}
+
+export function toTable(runs: PublicRunningStatus[]) {
+  return runs.map(toTableRun);
+}
+
+export function toTableRun(run: PublicRunningStatus) {
+  const runRate = run.run_rate !== undefined ? `${(run.run_rate * 60 * 60 * 24).toFixed(2)} s/day` : "-";
+  const cpu = run.cpu?.Value !== undefined && run.cpu_max?.Value !== undefined ? `${(run.cpu.Value).toFixed(0)}/${(run.cpu_max.Value).toFixed(0)}%` : "-";
+  const memory = run.memory?.Value !== undefined && run.memory_max?.Value !== undefined ? `${(run.memory.Value / 1024 / 1024 / 1024).toFixed(2)}/${(run.memory_max.Value / 1024 / 1024 / 1024).toFixed(2)} GiB` : "-";
+  return {
+    run_id: run.run_id,
+    account_id: run.account_id,
+    chid: run.chid,
+    cpu,
+    memory,
+    runRate,
+  }
+}
+
+// TODO: this is a polyfill and should be removed when possible.
+function readableStreamFromAsyncIterator<T>(iterator: AsyncIterableIterator<T>,): ReadableStream<T> {
+  return new ReadableStream({
+    async pull(controller) {
+      const { value, done } = await iterator.next();
+      if (done) { controller.close(); } else { controller.enqueue(value); }
+    },
+  });
+}
